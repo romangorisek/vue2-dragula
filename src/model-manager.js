72a28d734abc33cb2b3eaec8cbb39138f6c6e3f4
Kristian Mandrup <kmandrup@gmail.com>
2016-12-01 21:05:12 +0100

    fix model manager to align with demo app

--- src/model-manager.js
+++ src/model-manager.js
@@ -1,58 +1,70 @@
 export class ModelManager {
-  constructor(opts = {}) {
+  constructor (opts = {}) {
+    if (Array.isArray(opts)) {
+      opts = {
+        model: opts
+      }
+    }
     this.opts = opts
     this.name = opts.name
     this.drake = opts.drake
-    this.model = this.createModel(opts.model || [])
+
     this.history = opts.history || this.createHistory()
+
+    this.model = this.createModel(opts.model || [])
+    this.addToHistory(this.model)
+
     this.logging = opts.logging
     this.timeIndex = 0
     this.log('CREATE', opts)
   }
 
-  get clazzName() {
+  get clazzName () {
     return this.constructor.name || 'ModelManager'
   }
 
-  get shouldLog() {
+  get shouldLog () {
     return this.logging && this.logging.modelManager
   }
 
-  log(event, ...args) {
+  log (event, ...args) {
     if (!this.shouldLog) return
     console.log(`${this.clazzName} [${this.name}] :`, event, ...args)
   }
 
-  undo() {
+  undo () {
     this.log('undo', 'not yet implemented')
   }
 
-  redo() {
+  redo () {
     this.log('redo', 'not yet implemented')
   }
 
-  addToHistory(model) {
+  addToHistory (model) {
+    if (Array.isArray(model)) {
+      model = this.createFor({model: model})
+    }
     this.history.push(model)
     this.timeIndex++
   }
 
-  at(index) {
-    return this.model[index]
+  at (index) {
+    return this.model.get(index)
   }
 
-  createModel(model) {
-    return this.model || model || [];
+  createModel (model) {
+    return this.model || model || []
   }
 
-  createHistory() {
-    return this.history || [];
+  createHistory () {
+    return this.history || []
   }
 
-  createFor(opts = {}) {
+  createFor (opts = {}) {
     return new ModelManager(opts)
   }
 
-  removeAt(index) {
+  removeAt (index) {
     this.log('removeAt', {
       model: this.model,
       index
@@ -60,7 +72,7 @@ export class ModelManager {
     return this.model.splice(index, 1)
   }
 
-  insertAt(index, dropModel) {
+  insertAt (index, dropModel) {
     this.log('insertAt', {
       model: this.model,
       index,
@@ -69,7 +81,7 @@ export class ModelManager {
     return this.model.splice(index, 0, dropModel)
   }
 
-  move({dragIndex, dropIndex}) {
+  move ({dragIndex, dropIndex}) {
     this.log('move', {
       model: this.model,
       dragIndex,
